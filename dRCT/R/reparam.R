bd8d8d45f42db2c7fa717455b43af4bc631c9b3d
Charlotte Mann <manncz@umich.edu>
2023-05-04 10:53:15 -0400

    *paired function updates*
    updates `reparam` to handle new data structure outputted by `pair` and 
    does not rely on indexes.

--- dRCT/R/reparam.R
+++ dRCT/R/reparam.R
@@ -1,19 +1,34 @@
 #' Reparameterize Paired Data
 #'
 #' This function takes a data frame that has been processed by the \code{pair} function and formats it such that 
-#' covariates are parameterized as the within pair differences and means for each covariate.
+#' covariates are parameterized as the within pair differences (treatment - control) and means for each covariate.
 #' @param assigned A matrix of pair experimental data that his been processed by the \code{pair} function.
 #' @export
 
 # re-parameterize the covariates
 reparam = function(assigned){
-  nvar = (ncol(assigned)-4)/2
-  k = ncol(assigned)
-  Z1 = assigned[,5:(5+nvar-1)]
-  Z2 = assigned[,(5+nvar):k]
-  assigned[,5:(5+nvar-1)] = Z1-Z2
-  assigned[,(5+nvar):k] = (Z1+Z2)/2
-  names(assigned)[5:k] = c(paste("D",1:nvar,sep = ""),paste("M",1:nvar,sep = ""))
-  return(assigned)
+  
+  covs_long = assigned %>%
+    group_by(P) %>%
+    pivot_longer(starts_with("Z"),
+                 names_to = c("var", "label"),
+                 values_to = "val",
+                 names_pattern = "(.*)(\\d)")%>%
+    mutate(dif = case_when(label == 1 ~ val,
+                           TRUE ~ -val)) %>%
+    group_by(P, var) %>%
+    summarize(avg = mean(val), dif = sum(dif))
+  
+  covs = covs_long %>%
+    pivot_wider(id_cols = P,
+                names_from = var,
+                values_from = c(avg, dif),
+                names_glue = "{var}_{.value}")
+  
+  out = assigned %>%
+    select(-starts_with("Z")) %>%
+    left_join(covs, by = "P")
+  
+  return(out)
 }
 
