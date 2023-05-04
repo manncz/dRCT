29d757a023b0873b66f75724174e36ec60c27826
Charlotte Mann <manncz@umich.edu>
2023-05-04 10:57:02 -0400

    *paired function updates*
    update variance calculation to incorporate unit sizes.

--- dRCT/R/p_loop_var.R
+++ dRCT/R/p_loop_var.R
@@ -1,17 +1,25 @@
 #' Variance Estimate for the P-LOOP Estimator
 #'
 #' This function implements the variance estimate for the P-LOOP estimator and is used within \code{p_loop}.
-#' @param assigned A matrix of pair experimental data that his been processed by the \code{pair} function
-#' @param a A vector of the treatment minus control differences for the pairs where the first unit is assigned to treatment.
-#' @param b A vector of the treatment minus control differences for the pairs where the second unit is assigned to treatment.
+#' @param assigned A matrix of pair experimental data that has been processed by the \code{pair} function
+#' @param v1 A vector of the predicted treatment minus control differences for the pairs where the first unit is assigned to treatment.
+#' @param v2 A vector of the predicted treatment minus control differences for the pairs where the second unit is assigned to treatment.
+#' @param n_assigned A matrix of cluster sizes that has been processed by the \code{pair} function
 #' #' @export
 
-p_loop_var = function(assigned,a,b){
-  W_a = assigned$Y1[assigned$Tr == 1] - assigned$Y2[assigned$Tr == 1]
-  W_b = assigned$Y2[assigned$Tr == 0] - assigned$Y1[assigned$Tr == 0]
-  S_a = sum((a[assigned$Tr == 1] - W_a)^2)
-  S_b = sum((b[assigned$Tr == 0] - W_b)^2)
-  N = nrow(assigned)
-  varhat = (S_a + S_b)/N^2
+p_loop_var = function(assigned,v1,v2,n_assigned){
+  n1 = n_assigned$n1
+  n2 = n_assigned$n2
+  
+  V1 = 2*((assigned$Y1*n1)[assigned$Tr == 1] - (assigned$Y2*n2)[assigned$Tr == 1])
+  V2 = 2*((assigned$Y2*n2)[assigned$Tr == 0] - (assigned$Y1*n1)[assigned$Tr == 0])
+  
+  S1 = sum((2*v1[assigned$Tr == 1] - V1)^2)
+  S2 = sum((2*v2[assigned$Tr == 0] - V2)^2)
+  
+  N = sum(n1,n2)
+  varhat = (S1 + S2)/N^2
+  
+  return(varhat)
 }
 
