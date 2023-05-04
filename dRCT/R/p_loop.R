e81ff997e83267c89c4c824302022de073875ffe
Charlotte Mann <manncz@umich.edu>
2023-05-04 10:55:52 -0400

    *paired function updates*
    update `p_loop` to incorporate unit weights/sizes and do some error 
    catching

--- dRCT/R/p_loop.R
+++ dRCT/R/p_loop.R
@@ -1,51 +1,53 @@
 #' P-LOOP Estimator
 #'
 #' Covariate-adjusted estimator of the average treatment effect in pair randomized experiments. Given a set of experimental data (including observed outcome, treatment assignment, pair assignments, and covariates), uses a method \code{pred} to impute the potential outcomes for each observation, which are then used to estimate the average treatment effect. 
-#' @param Y A vector of experimental outcomes.
+#' @param Y A vector of mean experimental outcomes for each unit.
 #' @param Tr The treatment assignment vector.
 #' @param Z A matrix or vector of covariates.
 #' @param P A vector encoding the pair assignments. Observations with the same value of \code{P} will be assumed to be in the same pair.
+#' @param n A vector encoding the cluster sizes.
 #' @param pred The prediction algorithm used to impute potential outcomes. By default, this is \code{p_rf_interp}, which uses random forests and interpolates between methods \code{p_rf_ab} and \code{p_rf_s} that treat the pairs as units or treats the individuals as units when making predictions. Another option is \code{p_ols_interp}, which uses linear regression. User written imputation methods may be used as well.
 #' @param ... Arguments to be passed to the imputation method.
 #' @export
-#' @examples
-#' N = 50 # number of pairs
-#' 
-#' # parameters for the data generating process
-#' b1 = 10
-#' b2 = -5
-#' p1 = 0.9
-#' p2 = 0.5
-#' E = rep(0:1,N)
-#' 
-#' Z = rbinom(2*N,1,ifelse(E == 0,p1,p2)) # covariates
-#' tau = -10 # treatment effect
-#' P = c(1:N,1:N) # pair assignments
-#' 
-#' # generate potential outcomes
-#' c = 80 + b1*E + b2*Z + rnorm(2*N,0,2)
-#' t = tau + c
-#' 
-#' # generate treatment assignments
-#' Tr = rep(0,N*2)
-#' treatments = rbinom(N,1,0.5)
-#' for(i in 1:N){
-#'   Tr[which(P == i)] = c(treatments[i],(1-treatments[i]))
-#' }
-#' 
-#' Y = ifelse(Tr == 1,t,c) # observed outcomes
-#' 
-#' # estimate the average treatment effect for the observed data Y, Tr, and Z
-#' print(p_loop(Y,Tr,Z,P,p_rf_ab)) # pairs are treated as units when imputing potential outcomes
-#' print(p_loop(Y,Tr,Z,P,p_rf_s)) # individuals are treated as units when imputing potential outcomes
-#' print(p_loop(Y,Tr,Z,P,p_rf_interp)) # interpolation between the two methods
 
-p_loop = function(Y,Tr,Z,P,pred = p_rf_interp,...){
-  assigned = pair(Y,Tr,Z,P)
-  ab = pred(assigned,...)
-  varhat = p_loop_var(assigned,ab[,1],ab[,2])
-  d = 0.5*(ab[,1] - ab[,2])
-  tauhat = mean(Y[Tr == 1] - Y[Tr == 0]) - mean((2*assigned$Tr-1)*d)
-  return(c(tauhat,varhat))
+p_loop = function(Y,Tr,Z=NULL,P,n=NULL,pred = p_simple,...){
+  #if n isn't provided assume units have size 1
+  if(is.null(n)){n = rep(1, length(Y))}
+  
+  #if covariates aren't provided, set as 1 as a placeholder
+  if(is.null(Z)){
+    Z = rep(1, length(Y))
+    message("Note: unadjusted estimator is automatically used (pred = p_simple) because no covariates were provided")
+    pred = p_simple
+  }
+  
+  Z <- as.matrix(Z)
+  colnames(Z) <- paste0("Z", 1:(ncol(Z)))
+  
+  # reshape data
+  pair_out = pair(Y,Tr,Z,P,n)
+  
+  # data with treatment unit first
+  ordered = pair_out$ordered
+  
+  # data as assigned with Tr indicator
+  assigned = pair_out$agg
+  n_assigned = pair_out$n_assigned
+  
+  # interpolate v1 and v2
+  v12 = pred(ordered, assigned=assigned, n_assigned=n_assigned, ...)
+  v1= v12$v1
+  v2= v12$v2
+  
+  # treatment effect estimate
+  d = v1-v2
+  V = 2*(assigned$Tr*(assigned$Y1*n_assigned$n1 - assigned$Y2*n_assigned$n2) + (1-assigned$Tr)*(assigned$Y2*n_assigned$n2 - assigned$Y1*n_assigned$n1))
+  tauhat = sum(V + (1-2*assigned$Tr)*d)/sum(n_assigned$n1, n_assigned$n2)
+  
+  # variance estimate
+  varhat = p_loop_var(assigned,v1,v2,n_assigned)
+  
+  return(list(tauhat = tauhat,
+              varhat = varhat))
 }
 
