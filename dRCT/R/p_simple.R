0dd9ec3cae32ab792517c07cb505155de11c4ab0
Charlotte <manncz@umich.edu>
2023-06-16 16:58:04 -0400

    add an option to the simple estimator to include the truth for simulation purposes

--- dRCT/R/p_simple.R
+++ dRCT/R/p_simple.R
@@ -7,7 +7,7 @@
 #' @param n_assigned A matrix of pair experimental data cluster sizes that has been processed by the \code{pair} function.
 #' @export
 
-p_simple <- function(ordered, assigned, n_assigned){
+p_simple <- function(ordered, assigned, n_assigned, true_sum = NULL){
   
   M <- nrow(assigned)
   v1 <- v2 <- rep(NA, M)
@@ -20,6 +20,8 @@ p_simple <- function(ordered, assigned, n_assigned){
     #this is not really necessary since d=0 in this case, but flows through to the variance estimator
     loo_mean_sum <- mean((ordered$Y1[-i] + ordered$Y2[-i])/2)
     loo_mean_dif <- mean((ordered$Y1[-i] - ordered$Y2[-i])/2)
+    
+    if(!isnull(true_sum)) loo_mean_sum = true_sum[i]
       
     v1[i] <- (n1[i] - n2[i])*loo_mean_sum + (n1[i] + n2[i])*loo_mean_dif
     v2[i] <- (n2[i] - n1[i])*loo_mean_sum + (n1[i] + n2[i])*loo_mean_dif
