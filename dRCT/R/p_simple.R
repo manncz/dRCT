ba86a7f83e4425d0162e257f110ebd7abd6b31be
Charlotte <manncz@umich.edu>
2023-06-22 16:30:31 -0400

    add an option to do full mean imputation (not LOO) for mean imputation estimator

--- dRCT/R/p_simple.R
+++ dRCT/R/p_simple.R
@@ -7,7 +7,7 @@
 #' @param n_assigned A matrix of pair experimental data cluster sizes that has been processed by the \code{pair} function.
 #' @export
 
-p_simple <- function(ordered, assigned, n_assigned, true_sum = NULL){
+p_simple <- function(ordered, assigned, n_assigned, loo = T, true_sum = NULL){
   
   M <- nrow(assigned)
   v1 <- v2 <- rep(NA, M)
@@ -15,19 +15,24 @@ p_simple <- function(ordered, assigned, n_assigned, true_sum = NULL){
   n1 = n_assigned$n1
   n2 = n_assigned$n2
   
+  mean_sum1 = mean_sum2 <- mean((ordered$Y1 + ordered$Y2)/2)
+  mean_dif1 = mean_dif2 <- mean((ordered$Y1 - ordered$Y2)/2)
+  
   for(i in 1:M){
     #if no differing weights, this will get us back to the simple difference estimator
     #this is not really necessary since d=0 in this case, but flows through to the variance estimator
-    loo_mean_sum1 = loo_mean_sum2 <- mean((ordered$Y1[-i] + ordered$Y2[-i])/2)
-    loo_mean_dif1 = loo_mean_dif2 <- mean((ordered$Y1[-i] - ordered$Y2[-i])/2)
+    if(loo){
+      mean_sum1 = mean_sum2 <- mean((ordered$Y1[-i] + ordered$Y2[-i])/2)
+      mean_dif1 = mean_dif2 <- mean((ordered$Y1[-i] - ordered$Y2[-i])/2)
+    }
     
     if(!is.null(true_sum)){
       loo_mean_sum1 = true_sum[i,1]
       loo_mean_sum2 = true_sum[i,2]
     }
     
-    v1[i] <- (n1[i] - n2[i])*loo_mean_sum1 + (n1[i] + n2[i])*loo_mean_dif1
-    v2[i] <- (n2[i] - n1[i])*loo_mean_sum2 + (n1[i] + n2[i])*loo_mean_dif2
+    v1[i] <- (n1[i] - n2[i])*mean_sum1 + (n1[i] + n2[i])*mean_dif1
+    v2[i] <- (n2[i] - n1[i])*mean_sum2 + (n1[i] + n2[i])*mean_dif2
     
   }
   
